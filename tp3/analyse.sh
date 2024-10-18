c961d601f5985b5085fbff4b9e02beaee75121de
ShanthakumarThanus <thanus.shanthakumar.sio@gmail.com>
2024-10-18 16:57:24 +0200

    rep 2 et 3

--- tp3/analyse.sh
+++ tp3/analyse.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-printf "Bonjour, vous avez rentrée " $#
-printf "Le nom du script est " $0
-printf "Le 3ème paramètre est " $3
-printf "Voici la liste des paramètres : " $@
+printf "Bonjour, vous avez rentrée $# \n"
+printf "Le nom du script est $0 \n"
+printf "Le 3ème paramètre est $3 \n"
+
+for var in "$@"
+do
+        printf "Voici la liste des paramètres : $var \n"
+done
